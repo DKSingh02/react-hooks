2d49ec4cf92eeef1d28b1f01418bdb999b8e5e45
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2020-02-23 09:55:42 +0530

    reducer hook added

--- src/ReactHooks.js
+++ src/ReactHooks.js
@@ -1,12 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useReducer } from "react";
 import { UserContext } from "./index";
 
 // Instead of using UserContext.Consumer and render props pattern
 // We can use useContext hook provided by react
 // useContext method will take context which we have created see example below
 
+const initialState = {
+  count: 0
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      };
+    case "decrement":
+      return {
+        count: state.count - 1
+      };
+    case "reset":
+      return initialState;
+    default:
+      return initialState;
+  }
+}
+
 export default function ReactHooks() {
   const value = useContext(UserContext);
+  const [state, despatch] = useReducer(reducer, initialState);
   // return (
   // <div>
   // <UserContext.Consumer>
@@ -21,6 +43,10 @@ export default function ReactHooks() {
   return (
     <div>
       <div> Hello, {value}</div>
+      <div> Count, {state.count}</div>
+      <button onClick={() => despatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => despatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => despatch({ type: "reset" })}>Reset</button>
     </div>
   );
 }
